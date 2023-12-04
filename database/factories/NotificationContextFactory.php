b4e4cd289f54693cb8c9c5a4526cc3fd04f21215
Chijioke Ibekwe <ibekwe.chijioke18@gmail.com>
2023-12-04 09:45:15 +0100

    feat: create notification context factory

---
+++ database/factories/NotificationContextFactory.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace ChijiokeIbekwe\Messenger\Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+use ChijiokeIbekwe\Messenger\Models\NotificationContext;
+
+class NotificationContextFactory extends Factory
+{
+    protected $model = NotificationContext::class;
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            //
+        ];
+    }
+}
