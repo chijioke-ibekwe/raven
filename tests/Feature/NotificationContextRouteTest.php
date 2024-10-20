010a4bec0869cac9cd55664e59b19fe19a694cd8
Chijioke Ibekwe <ibekwe.chijioke18@gmail.com>
2024-10-20 17:29:12 +0100

    feat: move notification channel to notification context model

--- tests/Feature/NotificationContextRouteTest.php
+++ tests/Feature/NotificationContextRouteTest.php
@@ -3,7 +3,6 @@
 namespace ChijiokeIbekwe\Raven\Tests\Feature;
 
 use Illuminate\Foundation\Testing\RefreshDatabase;
-use ChijiokeIbekwe\Raven\Models\NotificationChannel;
 use ChijiokeIbekwe\Raven\Models\NotificationContext;
 use ChijiokeIbekwe\Raven\Tests\TestCase;
 use ChijiokeIbekwe\Raven\Tests\Utilities\User;
@@ -16,34 +15,28 @@ class NotificationContextRouteTest extends TestCase
     {
         // run the up() method (perform the migration)
         (new \CreateNotificationContextsTable)->up();
-        (new \CreateNotificationChannelsTable)->up();
-        (new \CreateNotificationChannelNotificationContextTable)->up();
     }
 
     public function test_that_authorized_users_can_successfully_fetch_all_notification_contexts ()
     {
-
         $user = User::factory(1)->make([
             'name' => 'John Doe',
             'email' => 'john.doe@raven.com'
         ])->get(0);
 
-        $context = NotificationContext::factory(1)->create([
+        NotificationContext::factory(1)->create([
             'name' => 'user-verified',
             'in_app_template_filename' => 'verification.json',
-            'type' => 'user'
+            'type' => 'user',
+            'channels' => ['DATABASE']
         ])->get(0);
-
-        $channel = NotificationChannel::where('type', 'DATABASE')->first();
-
-        $context->notification_channels()->attach($channel->id);
 
         $response = $this->actingAs($user)->getJson(route('contexts.index'));
 
         $response->assertStatus(200);
         $response->assertJson([
-            'status' => true,
-            'message' => 'Success',
+            'status' => 'success',
+            'message' => 'Notification contexts retrieved successfully',
             'data' => [
                 [
                     'id' => 1,
@@ -53,12 +46,7 @@ class NotificationContextRouteTest extends TestCase
                     'in_app_template_filename' => 'verification.json',
                     'sms_template_filename' => null,
                     'type' => 'user',
-                    'notification_channels' => [
-                        [
-                            'id' => 2,
-                            'type' => 'DATABASE'
-                        ]
-                    ]
+                    'channels' => ['DATABASE']
                 ]
             ]
         ]);
